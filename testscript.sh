ce830705a5c72ab0c1b334ddc20c6d043069fe8f
Christopher Orisawayi <chrisorisawayi@users.noreply.github.com>
2021-02-10 21:59:22 +0000

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 echo "jenkins scm test - parameterised"
 echo "First number is $NUMBER_ONE"
 echo "Second number is $NUMBER_TWO"
-SUM=$($NUMBER_ONE + $NUMBER_TWO)
+SUM="$NUMBER_ONE + $NUMBER_TWO"
 echo "Sum of both numbers is $SUM"
